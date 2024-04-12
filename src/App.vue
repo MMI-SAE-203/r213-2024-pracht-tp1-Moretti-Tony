53fdd9175147ced6147c2d31b39ef29f14d66442
Moretti Tony <tony.moretti@edu.univ-fcomte.fr>
2024-04-12 16:23:43 +0200

    commit v-show

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
 })
+
+const menuIsOpen = ref(false)
 </script>
 
 <template>
   <header>
-    <nav>
-      <ul>
+    <button
+  aria-controls="mainNav"
+  aria-expanded="true"
+  class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+>
+  menu
+</button>
+<!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
+<nav v-show="menuIsOpen" id="mainNav">
+  <ul>
+        <li>
+          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
+        </li>
         <li>
           <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
         </li>
       </ul>
-    </nav>
+</nav>
+
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>
